fcb9fbdabc1a48a416658eb23e640c18c2121b66
Rachel Warbelow <rachelwarbelow@gmail.com>
2014-12-31 16:48:04 -0700

    refactoring

--- lib/evaluator.rb
+++ lib/evaluator.rb
@@ -1,7 +1,8 @@
 class Evaluator
 	attr_reader :printer,
-							:game_in_progress,
-							:number
+							:game_in_progress
+
+	attr_accessor :number
 
 	def initialize(printer)
 		@printer = printer
@@ -11,7 +12,6 @@ class Evaluator
 	def execute(input)
 		if !game_in_progress
 			game_menu(input)
-			[printer.take_guess, :continue]
 		else
 			play_game(input)
 		end
@@ -21,6 +21,7 @@ class Evaluator
 		if input == 'p'
 			@number = rand(1..5)
 			@game_in_progress = true
+			[printer.take_guess, :continue]
 		elsif input == 'q'
 			[printer.quit_game, :stop]
 		end
@@ -28,16 +29,29 @@ class Evaluator
 
 	def play_game(input)
 		if input == 'q'
+			@game_in_progress = false
 			return [printer.end_round, :continue]
 		end
 		guess = input.to_i
-		if guess > number
+		if too_high?(guess)
 			[printer.too_high, :continue]
-		elsif guess < number
+		elsif too_low?(guess)
 			[printer.too_low, :continue]
-		elsif guess == number
+		elsif correct?(guess)
 			@game_in_progress = false
 			[printer.correct_guess, :continue]
 		end
+	end
+
+	def too_high?(guess)
+		guess > number
+	end
+
+	def too_low?(guess)
+		guess < number
+	end
+
+	def correct?(guess)
+		guess == number
 	end
 end
